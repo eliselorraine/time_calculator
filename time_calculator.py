bd72f7d8b453a7eee51c8345789eea3eabb0006f
Elise Lawrence <eliselorraine@icloud.com>
2022-06-29 16:06:02 +0200

    fighting with regex

--- time_calculator.py
+++ time_calculator.py
@@ -1,17 +1,34 @@
-def add_time(start, duration, day=None):
-    # add duration time to start time and return the result
-    # if result is the next day, it should show "next day" after the time
-    # if the result is more than one day later, it should show "n days later" after the time
+import re
 
-    # maybe we could convert to military time and then go back? 
+def add_time(start, duration, day=None):
 
     start_hr = int(start.split(':')[0])
     start_min = int(start.split(':')[1].split(' ')[0])
     time_of_day = start.split(' ')[1]
     next_day = False
+    new_day = day
 
     duration_hr = int(duration.split(':')[0])
     duration_min = int(duration.split(':')[1])
+
+    if day is not None and duration_hr >= 24 :
+        print(day)
+        days_of_week = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+        # look up what day it is in the dictionary. remember case insensitivity
+        
+        for d in days_of_week :
+            d = d.lower()
+            param_day = day.lower()
+            index = d.find(param_day)
+            print(index)
+        # use the lookup to grab the index of the day it is
+        # add that index to the days passed. 
+
+        day_n = int(duration_hr / 24)
+        while day_n > 7 :
+            day_n -= 7
+        new_day = days_of_week[day_n]
+        print(new_day)
 
     new_time_hr = start_hr + duration_hr
     new_time_min = start_min + duration_min
@@ -21,13 +38,13 @@ def add_time(start, duration, day=None):
         new_time_hr += 1
 
     if duration_hr >= 24:
-        n = -1
+        n = int(duration_hr / 24) + 1
+        if duration_hr == 24 and duration_min == 0:
+            n = 1
         while new_time_hr >= 12 :
             if time_of_day == 'AM':
                 time_of_day = 'PM'
-                n += 1
             else :
-                n += 1
                 time_of_day = 'AM'
             if new_time_hr == 12 :
                 break
@@ -57,7 +74,9 @@ def add_time(start, duration, day=None):
     if len(str_new_time_min) == 1:
         str_new_time_min = "0" + str_new_time_min
 
-    if next_day :
+    if day is not None and next_day != True :
+        new_time = f"{new_time_hr}:{str_new_time_min} {time_of_day}, {new_day}"
+    elif day is None and next_day :
         new_time = f"{new_time_hr}:{str_new_time_min} {time_of_day} (next day)"
     else :
         new_time = f"{new_time_hr}:{str_new_time_min} {time_of_day}"
